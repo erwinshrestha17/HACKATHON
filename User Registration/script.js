b8d4bc17f3e7fcfe27a50a7a91210830c5c5bf7b
DELL <erwinshrestha17@gmai.com>
2022-03-30 22:33:04 +0545

    comit

--- User Registration/script.js
+++
@@ -1,7 +0,0 @@
-const firstname = document.getElementById("firstname");
-const lastname = document.getElementById("lastname");
-const email = document.getElementById("email");
-const phoneNumber = document.getElementById("phoneNumber");
-const dateOfBirth = document.getElementById("dateofbirth");
-const btn = document.getElementById("btn");
-
